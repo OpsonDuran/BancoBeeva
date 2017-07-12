7de1cb21310f779125f456bc893e55782f3c641f
Armando Duran Salvador <iccarmandoduran@gmail.com>
2017-07-12 00:37:14 -0500

    anexo

--- src/main/java/com/beeva/banco/BancoBeeva/App.java
+++ src/main/java/com/beeva/banco/BancoBeeva/App.java
@@ -5,10 +5,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.beeva.banco.BancoBeeva.dao.BancoDao;
 import com.beeva.banco.BancoBeeva.dao.ClienteDao;
+import com.beeva.banco.BancoBeeva.dao.CuentaDao;
+import com.beeva.banco.BancoBeeva.dao.TipoCuentaDao;
 import com.beeva.banco.BancoBeeva.daoImpl.BancoDaoImpl;
 import com.beeva.banco.BancoBeeva.daoImpl.ClienteDaoImpl;
+import com.beeva.banco.BancoBeeva.daoImpl.CuentaChequesDaoImpl;
+import com.beeva.banco.BancoBeeva.daoImpl.TipoCuentaDaoImpl;
 import com.beeva.banco.BancoBeeva.entity.Banco;
 import com.beeva.banco.BancoBeeva.entity.Cliente;
+import com.beeva.banco.BancoBeeva.entity.Cuenta;
+import com.beeva.banco.BancoBeeva.entity.TipoCuenta;
+import com.beeva.banco.BancoBeeva.factory.CuentaFactory;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -26,7 +33,7 @@ public class App
     	ApplicationContext context= new ClassPathXmlApplicationContext("core-context.xml");
     	Banco banco = new Banco();
     	BancoDao bancoDao =(BancoDao) context.getBean(BancoDaoImpl.class);
-    	banco.setNombre("Banorte");
+    	banco.setNombre("Bankote");
     	int id=bancoDao.saveBanco(banco).getIdBanco();
     	String nombre=bancoDao.getBanco(id).getNombre();
     	int ids=bancoDao.getBanco(id).getIdBanco();
@@ -34,8 +41,21 @@ public class App
     	
     	Cliente cliente= new Cliente();
     	ClienteDao clienteDao=(ClienteDao) context.getBean(ClienteDaoImpl.class);
-    	cliente.setNombre("Juan");
-    	cliente.setApellido("Perez");
+    	cliente.setNombre("Jose");
+    	cliente.setApellido("Larios");
     	clienteDao.saveCliente(cliente);
+    	
+    	TipoCuenta tipoCuenta =new TipoCuenta();
+    	TipoCuentaDao tipoDao=(TipoCuentaDao)context.getBean(TipoCuentaDaoImpl.class);
+    	tipoCuenta.setNombre("Debito");
+    	int idtc= tipoDao.saveTipoCuenta(tipoCuenta).getIdtipocuenta();
+    	
+    	Cuenta cuenta= new Cuenta();
+    	CuentaDao cuentaDao=(CuentaDao)context.getBean(CuentaChequesDaoImpl.class);
+    	cuenta.setBalance(10000);
+    	cuenta.setIdCliente(ids);
+    	cuenta.setIdTipoCuenta(idtc);
+    	cuentaDao.saveCuenta(cuenta);
+    	
     }
 }
